b5e6bbbda4317e016a0ab9bbd174c5181f13bd23
acoulibaly01 <amadou.coulibaly@insa-strasbourg.fr>
2023-03-08 15:44:20 +0100

    Mise à jour de TestGitHub

---
+++ src/main/java/fr/insa/coulibaly/testgithub/TestGitHub.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package fr.insa.coulibaly.testgithub;
+
+/**
+ *
+ * @author Amadou Coulibaly
+ */
+public class TestGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
